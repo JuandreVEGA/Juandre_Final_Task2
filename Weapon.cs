e17bf5050d0a38253dce17e178185f894ff3ce36
JuandreVEGA <dkockjf@gmail.com>
2020-11-26 10:29:23 +0200

    Starting with POE

---
+++ Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    abstract class Weapon : Item
+    {
+        protected int Damage;
+        protected int Range;
+        protected int Durability;
+        protected int Cost;
+        protected string WeaponType;
+
+        public Weapon(string symbol, int x, int y) : base(x, y)
+        {
+
+        }
+    }
+}
